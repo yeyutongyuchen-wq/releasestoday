5b0749a273ecb8b8b0d3365e63766c2862942f13
ngunet <yeyutongyuchen@gmail.com>
2026-09-07 16:24:11 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   site: 'https://releasestoday.pages.dev',
   output: 'static',
